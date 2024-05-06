d3e437d35c05a0b27c61e6426a3f5bd81f156b02
vdymir <vladistar200302@gmail.com>
2024-05-06 16:21:34 -0500

    feat: header, about me and skills section

---
+++ src/shared/langs/en.js
@@ -0,0 +1,20 @@
+const i18mEN = {
+  home: "Home",
+  experience: "Experience",
+  contact_me: "Contact me",
+  blog: "Blog",
+  hello: "Hello! I'm",
+  vladimir: "Vladimir",
+  castaneda: "Castañeda",
+  see_cv: "See cv",
+  about_me: "About me",
+  resume: `
+  After 3 years in software development, the motivation behind my passion
+  for software engineering is a strong belief that programming is a means
+  for self-expression. When I design an app, I enjoy the process of
+  finding code that is creative, elegant, and practical. I want to immerse
+  myself in a community of people campaigning for innovation and
+  collaboration. I have a customer service mindset with a strong bias
+  toward action.
+  `,
+};
