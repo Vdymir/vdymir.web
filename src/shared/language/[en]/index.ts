8b86ce8026dc7e2263b15119ff4effd4371308a7
vdymir <vladistar200302@gmail.com>
2024-06-09 18:41:21 -0500

    feat: finish website 1.0

---
+++ src/shared/language/[en]/index.ts
@@ -0,0 +1,26 @@
+export const I18nEN = {
+  about_me: "About me",
+  skills: "Skills",
+  projects: "Projects",
+  contact_me: "Contact me",
+  hello_i_am: "Hello! I'm",
+  vladimir: "Vladimir",
+  Castaneda: "Castañeda",
+  software_developer: "Software developer",
+  see_cv: "See cv",
+  mi_description: `I am Vladimir Castañeda, a passionate software developer with extensive experience in frontend and mobile development. I have worked with technologies such as React, React Native, HTML5, CSS3, JavaScript, and Typescript, focusing on creating innovative and user-centric solutions. My experience includes developing mobile and web applications using modern frameworks and applying best practices and design patterns to ensure efficient and scalable products.
+    I have a solid understanding of microservices architecture and API integration, and I am a continuous learner, always staying up-to-date with the latest technological trends. My collaborative approach has enabled me to work effectively in multidisciplinary teams, contributing to project success through effective communication and a commitment to quality.
+    I am driven by solving complex problems and creating exceptional user experiences, always seeking to add value to the projects I participate in. My ability to adapt to dynamic environments and my passion for innovation make me a professional capable of meeting the challenges of modern software development.`,
+  on_site_description:
+    "On Site App transforms the way you explore our menu. Immerse yourself in a mouthwatering visual journey with high-definition images of every dish.",
+  rn_vdy_components_description:
+    "I have developed a library of components for React Native, designed to streamline the development process and enhance the functionality. Includes versatile components that cater to various common needs, ensuring a robust and efficient development experience.",
+  nails_description:
+    "Application to make reservations at a beauty salon, it has an administrative panel so that the owner can manage the scheduled appointments.",
+  email: "Email",
+  subject: "Subject",
+  description: "Description",
+  your_message: "Your message",
+  send: "Send",
+  footer_text: "Web developed with Astro and Tailwind ❤️",
+};
---
+++ src/shared/language/[es]/index.ts
@@ -0,0 +1,26 @@
+export const I18nES = {
+  about_me: "Sobre mí",
+  skills: "Habilidades",
+  projects: "Proyectos",
+  contact_me: "Contáctame",
+  hello_i_am: "¡Hola! Soy",
+  vladimir: "Vladimir",
+  Castaneda: "Castañeda",
+  software_developer: "Desarrollador de software",
+  see_cv: "Ver CV",
+  mi_description: `Soy Vladimir Castañeda, un apasionado desarrollador de software con amplia experiencia en desarrollo frontend y móvil. He trabajado con tecnologías como React, React Native, HTML5, CSS3, JavaScript y Typescript, enfocándome en crear soluciones innovadoras y centradas en el usuario. Mi experiencia incluye el desarrollo de aplicaciones móviles y web utilizando marcos modernos y aplicando mejores prácticas y patrones de diseño para garantizar productos eficientes y escalables.
+      Tengo un sólido entendimiento de la arquitectura de microservicios y la integración de APIs, y soy un aprendiz continuo, siempre manteniéndome al día con las últimas tendencias tecnológicas. Mi enfoque colaborativo me ha permitido trabajar eficazmente en equipos multidisciplinarios, contribuyendo al éxito del proyecto a través de una comunicación efectiva y un compromiso con la calidad.
+      Me impulsa resolver problemas complejos y crear experiencias de usuario excepcionales, siempre buscando agregar valor a los proyectos en los que participo. Mi capacidad para adaptarme a entornos dinámicos y mi pasión por la innovación me convierten en un profesional capaz de enfrentar los desafíos del desarrollo de software moderno.`,
+  on_site_description:
+    "On Site App transforma la forma en que exploras nuestro menú. Sumérgete en un recorrido visual apetitoso con imágenes en alta definición de cada plato.",
+  rn_vdy_components_description:
+    "He desarrollado una biblioteca de componentes para React Native, diseñada para agilizar el proceso de desarrollo y mejorar la funcionalidad. Incluye componentes versátiles que atienden a diversas necesidades comunes, asegurando una experiencia de desarrollo robusta y eficiente.",
+  nails_description:
+    "Aplicación para hacer reservas en un salón de belleza, cuenta con un panel administrativo para que el propietario pueda gestionar las citas programadas.",
+  email: "Correo electrónico",
+  subject: "Asunto",
+  description: "Descripción",
+  your_message: "Tu mensaje",
+  send: "Enviar",
+  footer_text: "Web desarrollada con Astro y Tailwind ❤️",
+};
